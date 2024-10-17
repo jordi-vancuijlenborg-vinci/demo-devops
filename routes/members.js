6eb34313f2cd80cc8625a8cb4c8c0a84cc73eb59
diana.potupis <diana.potupis@student.vinci.be>
2024-10-17 17:33:13 +0200

    refactoring members.js

--- routes/members.js
+++ routes/members.js
@@ -6,12 +6,16 @@ const User = require('../models/User.js');
 router.get('/', (req, res, next) => {
     console.log("MEMBER INDEX");
     console.log("session variable : " + JSON.stringify(req.session));
-    if (req.session.login) {
-        const user = User.find(req.session.login);
-        res.render('members/index', { user });
-    }
-    else {
-        res.redirect('/users');
+    authenticateUser();
+
+    function authenticateUser() {
+        if (req.session.login) {
+            const user = User.find(req.session.login);
+            res.render('members/index', { user });
+        }
+        else {
+            res.redirect('/users');
+        }
     }
 });
 
